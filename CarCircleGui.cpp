afa69090ef09f54ad04dc18fe3f7edb0cc24527d
sinaaghli <sina.aghli@gmail.com>
2020-03-27 22:50:39 -0600

    linked example to project successfully

--- CarCircleGui.cpp
+++ CarCircleGui.cpp
@@ -1,8 +1,9 @@
 #include <simbullet/simbullet.h>
 
-int main(){
+int main(int argc, char *argv[]){
     simbullet a;
     a.cnt = 2;
     int b = a.dosth(a.cnt);
+    a.run_example();
     return b;
 }
